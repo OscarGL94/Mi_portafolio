0af99f5d3d193a532d8b62e8b551ceb0ae55f4e8
OscarGL94 <oscargl94@outlook.com>
2025-05-26 18:44:11 +0100

    Mi primer commit

---
+++ Proyecto_Sprint7 (app).py
@@ -0,0 +1,32 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+# Leer los datos
+df = pd.read_csv("BNB_sprint7.csv")
+
+# Encabezado
+st.header("Análisis de Datos de BNB")
+
+# Casilla de verificación para mostrar histograma
+if st.checkbox("Mostrar Histograma"):
+    fig_hist = px.histogram(
+        df,
+        x="Close",
+        nbins=50,
+        title="Distribución del Precio de Cierre de BNB",
+        labels={"Close": "Precio de cierre (USD)", "count": "Frecuencia"}
+    )
+    st.plotly_chart(fig_hist)
+
+# Casilla de verificación para mostrar gráfico de dispersión
+if st.checkbox("Mostrar Gráfico de Dispersión"):
+    fig_scatter = px.scatter(
+        df,
+        x="Close",
+        y="Volume",
+        title="Relación entre Precio de Cierre y Volumen Cotizado",
+        labels={
+            "Close": "Precio de cierre (USD)", "Volume": "Volumen cotizado (USD)"}
+    )
+    st.plotly_chart(fig_scatter)
